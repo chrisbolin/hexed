35a0c03b9d71f0a33dd12591543ce94c0e75247a
Chris Bolin <bolin.chris@gmail.com>
2016-08-24 20:20:46 -0400

    adding scss

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello!");
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => <h1>ok!</h1>;
+
+render(<App/>, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
+const EXCLUDE = /node_modules/;
+
 module.exports = {
-  entry: "./index.js",
+  entry: ["./index.js", "./index.scss"],
   output: {
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: EXCLUDE,
+        query: {
+          presets: ['react', 'es2015']
+        },
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+        exclude: EXCLUDE,
+      }      
+    ]
   },
   watch: true
 }
